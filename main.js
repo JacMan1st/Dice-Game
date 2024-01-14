a0b1243781d46ce6c8fd145f45791ee47fe776eb
Jack Mears <jack.mears@ymail.com>
2024-01-14 20:38:05 +0000

    added the score card and roll button, change js so can use button to roll now

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-
 let cube = document.getElementById('cube');
+let rollButton = document.getElementById('rollButton');
 
-angleArray = [[0,0,0],[-310,-362,-38],[-400,-320,-2],[135,-217,-88],[-224,-317,5],[-47,-219,-81],[-133,-360,-53]];
-//this array of degree that show the deffrent number 1 2 3 4 5 6 on cube ie
-cube.addEventListener('click',function(event){
+angleArray = [[0, 0, 0], [-310, -362, -38], [-400, -320, -2], [135, -217, -88], [-224, -317, 5], [-47, -219, -81], [-133, -360, -53]];
 
-    /* throwing animation */
+rollButton.addEventListener('click', function (event) {
     cube.style.animation = 'animate 1.3s linear';
 
     const randomAngle = Math.floor(Math.random() * 6) + 1;
-    //console.log(randomAngle);
-    cube.style.transform = 'rotateX('+angleArray[randomAngle][0]+'deg) rotateY('+angleArray[randomAngle][1]+'deg) rotateZ('+angleArray[randomAngle][2]+'deg)';
-    cube.style.transition = '1s linear'
+    console.log(randomAngle);
+    cube.style.transform = 'rotateX(' + angleArray[randomAngle][0] + 'deg) rotateY(' + angleArray[randomAngle][1] + 'deg) rotateZ(' + angleArray[randomAngle][2] + 'deg)';
+    cube.style.transition = '1s linear';
 
-    cube.addEventListener('animationend',function(event){
+    cube.addEventListener('animationend', function (event) {
         cube.style.animation = '';
     });
 });
